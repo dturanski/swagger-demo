637462426f1a422f687cd532503162ca7ef5ebe4
David Turanski <dturanski@pivotal.io>
2016-09-13 15:20:31 -0400

    Add @WebAppConfiguration

--- src/test/java/com/example/SwaggerDemoFailsTests.java
+++ src/test/java/com/example/SwaggerDemoFailsTests.java
@@ -5,9 +5,11 @@ import org.junit.runner.RunWith;
 import org.springframework.boot.test.autoconfigure.json.JsonTest;
 import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.test.context.junit4.SpringRunner;
+import org.springframework.test.context.web.WebAppConfiguration;
 
 @RunWith(SpringRunner.class)
 @SpringBootTest
+@WebAppConfiguration
 @JsonTest
 public class SwaggerDemoFailsTests {
 
