de09d8100531e64836b089cbc3b08468ef450781
Andy Wilkinson <awilkinson@pivotal.io>
2016-09-13 20:40:55 +0100

    Configure Swagger separately so that @JsonTest can be used

--- src/test/java/com/example/SwaggerDemoFailsTests.java
+++ src/test/java/com/example/SwaggerDemoFailsTests.java
@@ -8,8 +8,6 @@ import org.springframework.test.context.junit4.SpringRunner;
 import org.springframework.test.context.web.WebAppConfiguration;
 
 @RunWith(SpringRunner.class)
-@SpringBootTest
-@WebAppConfiguration
 @JsonTest
 public class SwaggerDemoFailsTests {
 
